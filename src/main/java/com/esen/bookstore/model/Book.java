c41036ace9b2640e992913e25a7e76580097321b
mlev25 <mlevente25@mailbox.unideb.hu>
2025-04-15 09:35:18 +0200

    Configure database + added entities

--- src/main/java/com/esen/bookstore/model/Book.java
+++ src/main/java/com/esen/bookstore/model/Book.java
@@ -4,11 +4,60 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.Data;
 
-@Data
+
 @Entity
 public class Book {
+
+    public Book(){}
+
+    public Book(long id, String author, String publisher, Double price, String title) {
+        this.id = id;
+        this.author = author;
+        this.publisher = publisher;
+        this.price = price;
+        this.title = title;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -16,5 +65,7 @@ public class Book {
     private String author;
     private String publisher;
     private Double price;
+    private String title;
+
 
 }
--- src/main/java/com/esen/bookstore/model/BookStore.java
+++ src/main/java/com/esen/bookstore/model/BookStore.java
@@ -3,9 +3,61 @@ package com.esen.bookstore.model;
 import jakarta.persistence.*;
 import lombok.Data;
 
-@Data
+import java.util.Map;
+
 @Entity
 public class BookStore {
+
+    public BookStore(){}
+
+    public BookStore(long id, String location, Double priceModifier, Double moneyInCashRegister, Map<Book, Integer> inventory) {
+        this.id = id;
+        this.location = location;
+        this.priceModifier = priceModifier;
+        this.moneyInCashRegister = moneyInCashRegister;
+        this.inventory = inventory;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public Double getPriceModifier() {
+        return priceModifier;
+    }
+
+    public void setPriceModifier(Double priceModifier) {
+        this.priceModifier = priceModifier;
+    }
+
+    public Double getMoneyInCashRegister() {
+        return moneyInCashRegister;
+    }
+
+    public void setMoneyInCashRegister(Double moneyInCashRegister) {
+        this.moneyInCashRegister = moneyInCashRegister;
+    }
+
+    public Map<Book, Integer> getInventory() {
+        return inventory;
+    }
+
+    public void setInventory(Map<Book, Integer> inventory) {
+        this.inventory = inventory;
+    }
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
@@ -14,4 +66,7 @@ public class BookStore {
     private String location;
     private Double priceModifier;
     private Double moneyInCashRegister;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    private Map<Book, Integer> inventory;
 }
